afc8d9f1bbb36432ccef0bffd014d5d1882f1fa5
toof-jp <toof.jp.mail@gmail.com>
2019-06-27 16:19:51 +0900

    verified modpow

--- modpow.cpp
+++ modpow.cpp
@@ -1,15 +1,15 @@
-// verified https://atcoder.jp/contests/abc129/submissions/6133229
+// verified https://onlinejudge.u-aizu.ac.jp/solutions/problem/NTL_1_B/review/3692017/toof/C++14
 
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vl = vector<ll>;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
 #define rep(i, n) for(ll i = 0;i < n;i++)
 #define all(i) i.begin(), i.end()
-template<class t, class u> bool cmax(t& a, u b) { if (a<b) {a = b; return true;} else return false; }
-template<class t, class u> bool cmin(t& a, u b) { if (a>b) {a = b; return true;} else return false; }
+template<class T, class U> void cmax(T& a, U b) { if (a<b) a = b; }
+template<class T, class U> void cmin(T& a, U b) { if (a>b) a = b; }
 
 ll modpow(ll a, ll n, ll p) {
   ll res = 1;
@@ -24,20 +24,7 @@ ll modpow(ll a, ll n, ll p) {
 int main() {
   cin.tie(0); ios::sync_with_stdio(false);
 
-  string l;
-  cin >> l;
-  int n = l.size();
-
-  const ll mod = 1e9+7;
-  ll ans = 0;
-  int bit_cnt = 0;
-
-  rep(i, n) {
-    if (l[i] == '1') {
-      ans = (ans + modpow(2, bit_cnt, mod)*modpow(3, n-(i+1), mod)) % mod;
-      bit_cnt++;
-    }
-  }
-  ans = (ans + modpow(2, bit_cnt, mod)) % mod;
-  cout << ans << endl;
+  int m, n;
+  cin >> m >> n;
+  cout << modpow(m, n, 1e9+7) << endl;
 }
