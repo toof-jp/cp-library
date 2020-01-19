de3e3053fc301d19e23d120b1ebc42a1c1b886a9
toof-jp <toof.jp.mail@gmail.com>
2020-01-19 18:39:19 +0900

    update cumulative_sum

--- cumulative_sum.cpp
+++ cumulative_sum.cpp
@@ -15,28 +15,37 @@ struct CumulativeSum {
   vector<T> v;
 
   CumulativeSum(size_t n) : v(n+1) {};
-  CumulativeSum(vector<T> v_) : v(v_) {
-    v.push_back(T());
+  CumulativeSum(vector<T> v_) {
+    v.resize(v_.size()+1);
+    for (size_t i = 0; i < v_.size(); i++) v[i+1] = v_[i];
   };
 
   void add(size_t i, T x) {
-    v[i] += x;
+    v[i+1] += x;
   }
 
   // O(N)
   void build() {
-    for (size_t i = v.size()-1; i > 0; i--)
-      v[i-1] += v[i];
+    for (size_t i = 0; i < v.size()-1; i++)
+      v[i+1] += v[i];
   }
 
   // O(1) sum [l, r]
   T sum(size_t l, size_t r) const {
-    return v[l]-v[r+1];
+    return l == 0 ? v[r+1] : v[r+1]-v[l];
   }
 
   // O(1) sum [0, r]
   T sum(size_t r) const {
-    return sum(0, r);
+    return v[r+1];
+  }
+
+  T& operator[](size_t i) {
+    return v[i];
+  }
+
+  const T& operator[](size_t i) const {
+    return v[i];
   }
 };
 
