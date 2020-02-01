29bd70526fead9a409a265be47a333f4c8c0a1b8
toof-jp <toof.jp.mail@gmail.com>
2020-02-01 22:34:58 +0900

    update modint

--- modint.cpp
+++ modint.cpp
@@ -12,33 +12,16 @@ template<class T, class U> bool cmin(T& a, U b) { if (a>b) {a = b; return true;}
 template <ll Mod>
 struct ModInt {
   ll n;
-
   ModInt(const ll x = 0) : n(x) { while (n < 0) n += Mod; n %= Mod; }
   ModInt operator+(const ModInt r) const { return ModInt(*this) += r; }
   ModInt operator-(const ModInt r) const { return ModInt(*this) -= r; }
   ModInt operator*(const ModInt r) const { return ModInt(*this) *= r; }
   ModInt operator/(const ModInt r) const { return ModInt(*this) /= r; }
-  ModInt &operator+=(const ModInt r) {
-    n += r.n;
-    if (n >= Mod) n -= Mod;
-    return *this;
-  }
-  ModInt &operator-=(const ModInt r) {
-    if (n < r.n) n += Mod;
-    n -= r.n;
-    return *this;
-  }
+  ModInt &operator+=(const ModInt r) { n += r.n; if (n >= Mod) n -= Mod; return *this; }
+  ModInt &operator-=(const ModInt r) { if (n < r.n) n += Mod; n -= r.n; return *this; }
   ModInt &operator*=(const ModInt r) { n = n * r.n % Mod; return *this; }
   ModInt &operator/=(const ModInt r) { return *this *= r.inv(); }
-  ModInt pow(ll x) const {
-    ModInt<Mod> ret(1), tmp(*this);
-    while (x) {
-      if (x&1) ret *= tmp;
-      tmp *= tmp;
-      x >>= 1;
-    }
-    return ret;
-  }
+  ModInt pow(ll x) const { ModInt<Mod> ret(1), tmp(*this); while (x) { if (x&1) ret *= tmp; tmp *= tmp; x >>= 1; } return ret; }
   ModInt inv() const { return pow(Mod-2); }
   friend ostream& operator<<(ostream& os, const ModInt& obj) { return os << obj.n; }
   friend istream& operator>>(istream& is, ModInt& obj) { ll t; is >> t; obj = ModInt(t); return is; }
