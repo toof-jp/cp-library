35035e76d121fc18bc430ff7262e5c2c23e8d808
toof-jp <toof.jp.mail@gmail.com>
2020-02-01 21:45:38 +0900

    update modint

--- modint.cpp
+++ modint.cpp
@@ -10,28 +10,28 @@ template<class T, class U> bool cmax(T& a, U b) { if (a<b) {a = b; return true;}
 template<class T, class U> bool cmin(T& a, U b) { if (a>b) {a = b; return true;} else return false; }
 
 template <ll Mod>
-struct modint {
+struct ModInt {
   ll n;
 
-  modint(const ll x = 0) : n(x) { while (n < 0) n += Mod; n %= Mod; }
-  modint operator+(const modint r) const { return modint(*this) += r; }
-  modint operator-(const modint r) const { return modint(*this) -= r; }
-  modint operator*(const modint r) const { return modint(*this) *= r; }
-  modint operator/(const modint r) const { return modint(*this) /= r; }
-  modint &operator+=(const modint r) {
+  ModInt(const ll x = 0) : n(x) { while (n < 0) n += Mod; n %= Mod; }
+  ModInt operator+(const ModInt r) const { return ModInt(*this) += r; }
+  ModInt operator-(const ModInt r) const { return ModInt(*this) -= r; }
+  ModInt operator*(const ModInt r) const { return ModInt(*this) *= r; }
+  ModInt operator/(const ModInt r) const { return ModInt(*this) /= r; }
+  ModInt &operator+=(const ModInt r) {
     n += r.n;
     if (n >= Mod) n -= Mod;
     return *this;
   }
-  modint &operator-=(const modint r) {
+  ModInt &operator-=(const ModInt r) {
     if (n < r.n) n += Mod;
     n -= r.n;
     return *this;
   }
-  modint &operator*=(const modint r) { n = n * r.n % Mod; return *this; }
-  modint &operator/=(const modint r) { return *this *= r.inv(); }
-  modint pow(ll x) const {
-    modint<Mod> ret(1), tmp(*this);
+  ModInt &operator*=(const ModInt r) { n = n * r.n % Mod; return *this; }
+  ModInt &operator/=(const ModInt r) { return *this *= r.inv(); }
+  ModInt pow(ll x) const {
+    ModInt<Mod> ret(1), tmp(*this);
     while (x) {
       if (x&1) ret *= tmp;
       tmp *= tmp;
@@ -39,12 +39,13 @@ struct modint {
     }
     return ret;
   }
-  modint inv() const { return pow(Mod-2); }
-  friend ostream& operator<<(ostream& os, const modint& obj) { return os << obj.n; }
-  friend istream& operator>>(istream& is, modint& obj) { ll t; is >> t; obj = modint(t); return is; }
+  ModInt inv() const { return pow(Mod-2); }
+  friend ostream& operator<<(ostream& os, const ModInt& obj) { return os << obj.n; }
+  friend istream& operator>>(istream& is, ModInt& obj) { ll t; is >> t; obj = ModInt(t); return is; }
 };
 constexpr ll mod = 1000000007;
-using mint = modint<mod>;
+using mint = ModInt<mod>;
+mint operator"" _mi(unsigned long long n) { return mint(n); }
 
 int main() {
   cin.tie(0); ios::sync_with_stdio(false);
