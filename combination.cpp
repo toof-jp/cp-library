f7c302d684b2508f613047cd2f05ad203270ec9d
toof-jp <toof.jp.mail@gmail.com>
2020-03-18 05:36:13 +0900

    fix combination.cpp

--- combination.cpp
+++ combination.cpp
@@ -1,14 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vl = vector<ll>;
-#define rep(i, n) for(ll i = 0;i < n;i++)
-#define all(i) i.begin(), i.end()
-template<class T, class U> bool cmax(T& a, U b) { if (a<b) {a = b; return true;} else return false; }
-template<class T, class U> bool cmin(T& a, U b) { if (a>b) {a = b; return true;} else return false; }
-
 // T modint
 template<class T>
 struct Combination {
@@ -36,8 +25,3 @@ struct Combination {
     return C(n+k-1, n-1);
   }
 };
-
-int main() {
-  cin.tie(0); ios::sync_with_stdio(false);
-
-}
