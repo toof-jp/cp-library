4f7616af417ba2e2b7cd63cde9bcbdeb4ff80f99
toof-jp <toof.jp.mail@gmail.com>
2020-03-18 05:32:10 +0900

    add combination test

---
+++ test/DPL_5_D.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_5_D"
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+#define rep(i, n) for(ll i = 0;i < n;i++)
+#define all(i) i.begin(), i.end()
+template<class T, class U> bool cmax(T& a, U b) { if (a<b) {a = b; return true;} else return false; }
+template<class T, class U> bool cmin(T& a, U b) { if (a>b) {a = b; return true;} else return false; }
+
+#include "../modint.cpp"
+#include "../combination.cpp"
+
+int main() {
+  cin.tie(0); ios::sync_with_stdio(false);
+
+  ll n, k;
+  cin >> n >> k;
+  Combination<mint> c(n+k);
+  cout << c.H(k, n) << endl;
+}
