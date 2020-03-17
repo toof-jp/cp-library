4182b226430a5d806671991141b01353509714a1
toof-jp <toof.jp.mail@gmail.com>
2020-03-17 18:38:32 +0900

    add cumulative sum test

--- cumulative_sum.cpp
+++ cumulative_sum.cpp
@@ -1,15 +1,3 @@
-// https://onlinejudge.u-aizu.ac.jp/solutions/problem/0022/review/4031365/toof/C++14
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-using vl = vector<ll>;
-#define rep(i, n) for(ll i = 0;i < n;i++)
-#define all(i) i.begin(), i.end()
-template<class T, class U> bool cmax(T& a, U b) { if (a<b) {a = b; return true;} else return false; }
-template<class T, class U> bool cmin(T& a, U b) { if (a>b) {a = b; return true;} else return false; }
-
 template<class T>
 struct CumulativeSum {
   vector<T> v;
@@ -48,29 +36,3 @@ struct CumulativeSum {
     return v[i];
   }
 };
-
-int main() {
-  cin.tie(0); ios::sync_with_stdio(false);
-
-  while (true) {
-    ll n;
-    cin >> n;
-    if (n == 0) break;
-
-    vector<ll> v(n);
-    rep(i, n) {
-      cin >> v[i];
-    }
-
-    CumulativeSum<ll> c(v);
-    c.build();
-
-    ll ans = -1e10;
-    rep(i, n) {
-      for (ll j = i; j < n; j++) {
-        cmax(ans, c.sum(i, j));
-      }
-    }
-    cout << ans << endl;
-  }
-}
