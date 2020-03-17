9cb3063cc2a7486c0995c13868929b138d8780c0
toof-jp <toof.jp.mail@gmail.com>
2020-03-17 21:59:26 +0900

    add prime factorization test

--- prime_factorization.cpp
+++ prime_factorization.cpp
@@ -1,11 +1,3 @@
-//
-// https://beta.atcoder.jp/contests/abc052/tasks/arc067_a
-//
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define rep(i, n) for(ll i = 0;i < n;i++)
-
 map<ll, ll> PrimeFactorization(ll n) {
   map<ll, ll> m;
   for (int i = 2; i*i <= n; i++) {
@@ -16,26 +8,4 @@ map<ll, ll> PrimeFactorization(ll n) {
   }
   if (n != 1) m[n]++;
   return m;
-}
-
-int main() {
-  cin.tie(0); ios::sync_with_stdio(false);
-
-  int n;
-  cin >> n;
-
-  map<ll, ll> m;
-  rep(i, n) {
-    for (auto&& j : PrimeFactorization(i+1)) {
-      m[j.first] += j.second;
-    }
-  }
-
-  ll ans = 1;
-  const int MOD = (int)1e9+7;
-  for (auto&& i : m) {
-    ans = ans*(i.second+1)%MOD;
-  }
-
-  cout << ans << endl;
 }
