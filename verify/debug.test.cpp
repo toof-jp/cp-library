55c62f8e30b1b04a580a554ad3827bceb0864b4b
toof-jp <toof.jp.mail@gmail.com>
2020-04-14 02:21:31 +0900

    add debug.hpp

---
+++ verify/debug.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+
+#include "../template.hpp"
+#include "../debug.hpp"
+
+int main() {
+  ll n = 10;
+  double m = 3.141592;
+  bool f = true;
+
+  debug(n, m, f);
+
+  vector<int> a(n);
+  debug(a);
+
+  vector<vl> b(n, vl(n));
+  debug(b);
+
+  cout << "Hello World" << el;
+}
