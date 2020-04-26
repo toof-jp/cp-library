d44acbe9718f128f4bff3c87e1477d164880c91b
toof <toof.jp.mail@gmail.com>
2020-04-27 01:05:24 +0900

    Update template.hpp

--- template.hpp
+++ template.hpp
@@ -9,13 +9,13 @@ using vl = vector<ll>;
 #define per(i, n) for(ll i = (ll)n-1; i >= 0; i--)
 #define per3(i, l, r) for(ll i = (ll)r-1; i >= (ll)l; i--)
 #define all(v) begin(v), end(v)
-template<class T, class U> inline void cmax(T& a, U b) { if (a < b) a = b; }
-template<class T, class U> inline void cmin(T& a, U b) { if (a > b) a = b; }
+template<class T, class U> inline void cmax(T &a, U b) { if (a < b) a = b; }
+template<class T, class U> inline void cmin(T &a, U b) { if (a > b) a = b; }
 constexpr char el = '\n';
-template<class T, class U> ostream &operator<<(ostream &os, const pair<T, U>& p) { os << p.first << " " << p.second; return os; }
+template<class T, class U> ostream &operator<<(ostream &os, const pair<T, U>　&p) { os << p.first << " " << p.second; return os; }
 template<class T, class U> istream &operator>>(istream &is, pair<T, U> &p) { is >> p.first >> p.second; return is; }
-template<class T> ostream &operator<<(ostream &os, const vector< T > &v) { rep(i, v.size()) os << v[i] << (i + 1 != v.size() ? " " : ""); return os; }
-template<class T> istream &operator>>(istream &is, vector< T > &v) { for(T &in : v) is >> in; return is; }
+template<class T> ostream &operator<<(ostream &os, const vector<T> &v) { rep(i, v.size()) os << v[i] << (i+1 != v.size() ? " " : ""); return os; }
+template<class T> istream &operator>>(istream &is, vector<T> &v) { for(T &i : v) is >> i; return is; }
 struct IoSetup {
   IoSetup() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(15); cerr << fixed << setprecision(15); }
 } io_setup;
