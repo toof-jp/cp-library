4d0fd8e869624aed68d86da034ceb4da6d04ef90
toof-jp <toof.jp.mail@gmail.com>
2020-06-08 21:30:16 +0900

    ライブラリがバグってると困るんだよなぁ

--- prime_factorization.hpp
+++ prime_factorization.hpp
@@ -3,7 +3,7 @@
 
 map<ll, ll> prime_factorization(ll n) {
   map<ll, ll> m;
-  for (int i = 2; i*i <= n; i++) {
+  for (ll i = 2; i*i <= n; i++) {
     while (n%i == 0) {
       m[i]++;
       n /= i;
